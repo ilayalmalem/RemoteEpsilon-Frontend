4d7a6835086f893721d04ebb4a2ee338586327f8
Ilay David Almalem <ilayalmalem@gmail.com>
2021-01-11 19:51:22 +0200

    v1.1.0+2
    ## v1.1.0+2
    * Fix Navbar padding bug
    * Add Home page responsivity
    * Remove main.scss unused code

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,16 +1,14 @@
 import illustration from "../assets/main_illustration.svg";
-import {useRef} from "react";
-
 
 export default function HomePage(props) {
     return (
-        <div className="home-page text-black">
-            <div className="content-wrapper flex w-full h-full">
-                <div className="left-content h-4/6 flex flex-col justify-center w-1/2">
+        <div className="home-page flex flex-col items-center text-black">
+            <div className="content-wrapper flex h-full md:h-full w-full items-center lg:items-start">
+                <div className="left-content justify-center h-4/6 flex flex-col w-full md:w-1/2">
                     <div id="headline">
                         הכירו סוג חדש של למידה מרחוק.
                     </div>
-                    <div id="subheadline">
+                    <div id="subheadline" className="w-full md:w-7/12">
                         בואו לחוות סוג חדש של למידה מרחוק. כל הכלים הדרושים במקום אחד, בכל זמן, מכל מקום במקסימום נוחות.
                     </div>
 
@@ -18,12 +16,12 @@ export default function HomePage(props) {
                         props.scrollTo.current.scrollIntoView({
                             behavior: "smooth"
                         })
-                    }} id="cta">התחילו
+                    }} id="cta" className="w-full mt-8 md:w-1/4">התחילו
                     </button>
                 </div>
 
-                <div className="right-side flex mt-3 justify-end items-start w-1/2">
-                    <img className="w-10/12" src={illustration} alt=""/>
+                <div className="right-side hidden md:flex mt-3 justify-end items-start w-1/2">
+                    <img className="w-full md:w-10/12" src={illustration} alt=""/>
                 </div>
             </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,13 @@ import {Link} from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="navbar w-full">
-            <div className="navbar-content px-20 flex justify-center lg:justify-between items-center text-black">
+            <div className="navbar-content px-10 md:px-20 flex justify-between items-center text-black">
                 <Link className="no-underline" to="/">
-                    <div className="logo font-semibold">RemoteEpsilon</div>
+                    <div className="logo text-lg md:text-xl font-semibold">RemoteEpsilon</div>
                 </Link>
+                <div className="flex justify-center sm:justify-start md:hidden">
+                    <Link className="link text-sm md:text-base text-red-400 rounded-full text-white" to="/login">התחברות</Link>
+                </div>
                 <ul className="hidden md:flex link-list flex items-center">
                     <Link className="link" to="/teachers">למורים</Link>
                     <Link className="link" to="/students">לתלמידים</Link>
